f3f1610fdd08e00a48d25aed08f5c72560ae29b2
Pawel Lampe <pawel.lampe@gmail.com>
2021-08-24 23:14:57 +0200

    remove 'tool' support

--- tests/formatter/test_executable.py
+++ tests/formatter/test_executable.py
@@ -4,12 +4,12 @@ from ..common import write_file
 
 
 def test_valid_file(tmp_path):
-    dummy_file = write_file(tmp_path, "script.gd", "tool")
+    dummy_file = write_file(tmp_path, "script.gd", "pass")
     assert subprocess.run(["gdformat", dummy_file], check=False).returncode == 0
 
 
 def test_check_valid_file_to_reformat(tmp_path):
-    dummy_file = write_file(tmp_path, "script.gd", "tool;var x")
+    dummy_file = write_file(tmp_path, "script.gd", "pass;var x")
     assert (
         subprocess.run(["gdformat", "--check", dummy_file], check=False).returncode != 0
     )
